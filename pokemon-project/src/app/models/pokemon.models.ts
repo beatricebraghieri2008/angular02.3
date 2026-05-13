d7de0bf7e4bc269a32ce893a581d3df9e19cad70
beatricebraghieri2008 <braghieri.beatrice@iisgalvanimi.edu.it>
2026-05-13 10:54:43 +0000

    lavoro finito

--- pokemon-project/src/app/models/pokemon.models.ts
+++ pokemon-project/src/app/models/pokemon.models.ts
@@ -1,19 +1,23 @@
-// Per la lista delle categorie (tipi)
 export interface TypeResponse {
   results: { name: string; url: string }[];
 }
 
-// Per la lista dei Pokémon dentro un tipo
 export interface PokemonInType {
   pokemon: {
     pokemon: { name: string; url: string };
   }[];
 }
 
-// Per i dettagli del singolo Pokémon
 export interface PokemonDetails {
   name: string;
-  sprites: { front_default: string };
-  weight: number;
-  height: number;
+  sprites: {
+    front_default: string;
+  };
+  abilities: {
+    ability: { name: string };
+  }[];
+  stats: {
+    base_stat: number;
+    stat: { name: string };
+  }[];
 }
--- pokemon-project/src/app/type-list/type-list.ts
+++ pokemon-project/src/app/type-list/type-list.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PokemonService } from '../pokemon';
-import { TypeResponse, PokemonInType } from '../models/pokemon.models';
+import { TypeResponse, PokemonInType, PokemonDetails } from '../models/pokemon.models';
 
 @Component({
   selector: 'app-type-list',
@@ -12,37 +12,27 @@ import { TypeResponse, PokemonInType } from '../models/pokemon.models';
 })
 export class TypeListComponent implements OnInit {
   types: any[] = [];
-  pokemonList: any[] = []; // Array per la lista dei pokemon
-  selectedPokemon: any = null; // Per memorizzare il pokemon cliccato
-
-showDetails(url: string) {
-  this.pokemonService.getDetails(url).subscribe(data => {
-    this.selectedPokemon = data;
-  });
-}
+  pokemonList: any[] = []; 
+  selectedPokemon: any = null; 
 
   constructor(private pokemonService: PokemonService) {}
 
   ngOnInit(): void {
-    console.log("Richiedo le categorie...");
-    this.pokemonService.getTypes().subscribe({
-      next: (data: TypeResponse) => {
-        console.log("Categorie ricevute:", data);
-        this.types = data.results.slice(0, 6); // Prende i primi 6
-      },
-      error: (err) => console.error("Errore categorie:", err)
+    this.pokemonService.getTypes().subscribe((data: TypeResponse) => {
+        this.types = data.results.slice(0, 6);
     });
   }
 
-  // Metodo per caricare i pokemon di una categoria
   selectType(url: string) {
-    console.log("Carico pokemon dal tipo:", url);
-    this.pokemonService.getPokemonByType(url).subscribe({
-      next: (data: PokemonInType) => {
-        console.log("Pokemon ricevuti:", data);
+    this.selectedPokemon = null; 
+    this.pokemonService.getPokemonByType(url).subscribe((data: PokemonInType) => {
         this.pokemonList = data.pokemon;
-      },
-      error: (err) => console.error("Errore pokemon:", err)
+    });
+  }
+
+  showDetails(url: string) {
+    this.pokemonService.getDetails(url).subscribe((data: PokemonDetails) => {
+        this.selectedPokemon = data;
     });
   }
 }
